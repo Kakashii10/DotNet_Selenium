f82e1b7cf168a9d3bae669ba1669e027c3b4eb3f
mohitsinghnegi10 <mohitsingh10799@gmail.com>
2024-04-27 23:38:56 +0530

    first commit

---
+++ ConsoleApp4/Test.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Test
+    {
+        IWebDriver driver = null;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver(); 
+        }
+
+        [Test]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+
+            Console.WriteLine("shdsjdhs",driver);
+            driver.Quit();
+        }
+    }
+}
